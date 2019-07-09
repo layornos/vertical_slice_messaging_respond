6488e1a3dc08a39bc161479890cbcb6ae073de75
Sandro <layornos@gmail.com>
2019-07-09 11:25:31 +0200

    added visualization and webserver

---
+++ visualization/webserver/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("/Users/layornos/Projects/RESPOND/vertical_slice_messaging_respond/visualization/webpage")))
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
