63f9c13787ab94e16452e89eefb7ab6f853fe005
Adrian Freund <adrian@freund.io>
2020-04-13 12:56:42 +0200

    Modify model in bpmn2 demo

--- bpmn2_demo/src/main/kotlin/edu/kit/ipd/sdq/respond/bpmn2/Main.kt
+++ bpmn2_demo/src/main/kotlin/edu/kit/ipd/sdq/respond/bpmn2/Main.kt
@@ -5,6 +5,8 @@ import org.eclipse.bpmn2.util.Bpmn2ResourceFactoryImpl
 import org.eclipse.emf.common.util.URI
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 fun main() {
     val rs = ResourceSetImpl()
@@ -15,5 +17,10 @@ fun main() {
     val documentRoot = bpmnResource.contents[0] as DocumentRoot
 
     val startEvents = documentRoot.findStartEvents()
+    // Traverse the bpmn graph
     startEvents[0].traverse { println(it.name) }
+
+    //Make changes to the graph and save it
+    startEvents[0].name = "Changed"
+    bpmnResource.save(FileOutputStream("output.bpmn"), null)
 }
