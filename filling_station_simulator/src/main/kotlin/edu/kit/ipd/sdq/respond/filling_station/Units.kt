cfe5f958582670b6ab3201395bac29f17673fe65
Adrian Freund <adrian@freund.io>
2020-05-19 13:31:18 +0200

    Added new message formats to filling station simulator

--- filling_station_simulator/src/main/kotlin/edu/kit/ipd/sdq/respond/filling_station/Units.kt
+++ filling_station_simulator/src/main/kotlin/edu/kit/ipd/sdq/respond/filling_station/Units.kt
@@ -1,4 +1,10 @@
 package edu.kit.ipd.sdq.respond.filling_station
 
+import java.time.Instant
+
 typealias Millimeter = Int
 typealias Gram = Int
+
+val Instant.milli: Int
+    get() = this.nano / 1000
+
