15755a6d758b933f76705dad54db34845d00a6c6
Adrian Freund <adrian@freund.io>
2020-12-11 15:57:53 +0100

    Add github actions for automatic deploy

---
+++ .github/scripts/decrypt-secret.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output "$SECRET_NAME" "$SECRET_NAME.gpg"
